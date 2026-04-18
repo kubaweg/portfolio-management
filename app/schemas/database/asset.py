e77cd353f4f0fcd476028dc3d4c4336907669c81
Kuba Węgłowski <kubaweg01@gmail.com>
2026-04-18 23:52:50 +0200

    dev/changes in database models

--- app/schemas/database/asset.py
+++ app/schemas/database/asset.py
@@ -7,78 +7,89 @@ from datetime import date
 class AssetType(str, enum.Enum):
     ETF = "ETF"
     ETC = "ETC"
-    BOND = "BOND"
+    BOND = "Obligacja"
+    EQUITY = "Akcja"
 
 class Category1(str, enum.Enum):
-    EQUITY = "EQUITY"
-    MIXED = "MIXED"
-    BOND = "BOND"
-    COMMODITY = "COMMODITY"
-    CASH = "CASH"
+    EQUITY = "Akcje"
+    MIXED = "Mix"
+    BOND = "Obligacje"
+    COMMODITY = "Surowce/towary"
+    CASH = "Cash"
 
 class Category2(str, enum.Enum):
-    EQUITY_GLOBAL = "EQUITY_GLOBAL"
-    EQUITY_REGION = "EQUITY_REGION"
-    EQUITY_COUNTRY = "EQUITY_COUNTRY"
-    EQUITY_FACTOR = "EQUITY_FACTOR"
-    EQUITY_SECTOR = "EQUITY_SECTOR"
-    BOND_RETAIL_FIXED_RATE = "BOND_RETAIL_FIXED_RATE"
-    BOND_RETAIL_INFLATION_LINKED = "BOND_RETAIL_INFLATION_LINKED"
-    BOND_RETAIL_INTEREST_LINKED = "BOND_RETAIL_INTEREST_LINKED"
-    BOND_CORP_FIXED = "BOND_CORP_FIXED"
-    BOND_CORP_FLOATING = "BOND_CORP_FLOATING"
-    COMMODITY_GOLD = "COMMODITY_GOLD"
-    COMMODITY_SILVER = "COMMODITY_SILVER"
-    COMMODITY_BROAD = "COMMODITY_BROAD"
+    EQUITY_GLOBAL = "Akcje ogólnoświatowe"
+    EQUITY_REGION = "Akcje regionalne"
+    EQUITY_COUNTRY = "Akcje krajowe"
+    EQUITY_FACTOR = "Akcje faktorowe"
+    EQUITY_SECTOR = "Akcje sektorowe"
+    BOND_RETAIL_FIXED_RATE = "Obligacje skarbowe o stopie stałej"
+    BOND_RETAIL_INFLATION_LINKED = "Obligacje skarbowe indeksowane inflacją"
+    BOND_RETAIL_INTEREST_LINKED = "Obligacje skarbowe indeksowane stopą referencyjną"
+    BOND_CORP_FIXED = "Obligacje korporacyjne o stopie stałej"
+    BOND_CORP_FLOATING = "Obligacje skarbowe zmiennoprocentowe"
+    COMMODITY_GOLD = "Surowce - złoto"
+    COMMODITY_SILVER = "Surowce - srebro"
+    COMMODITY_BROAD = "Surowce - inne"
 
 # Naprawione: Tylko lokalizacje geograficzne
 class GeoRegion(str, enum.Enum):
-    GLOBAL = "GLOBAL"
-    EUROPE = "EUROPE"
-    ASIA_PACIFIC = "ASIA_PACIFIC"
-    NORTH_AMERICA = "NORTH_AMERICA"
-    POLAND = "POLAND"
+    GLOBAL = "Cały świat"
+    EUROPE = "Europa"
+    EUROPE_WEST = "Europa Zachodnia"
+    EUROPE_EAST = "Europa Wschodnia"
+    ASIA_PACIFIC = "Azja/Pacyfik"
+    NORTH_AMERICA = "Ameryka Północna"
+    SOUTH_AMERICA = "Ameryka Południowa"
+    AFRICA = "Afryka"
+    AUSTRALIA = "Australia"
+
+class GeoCountry(str, enum.Enum):
+    POLAND = "Polska"
     USA = "USA"
-    GERMANY = "GERMANY"
-    UK = "UK"
-    FRANCE = "FRANCE"
-    JAPAN = "JAPAN"
-    CHINA = "CHINA"
+    GERMANY = "Niemcy"
+    UK = "Wielka Brytania"
+    FRANCE = "Francja"
+    JAPAN = "Japonia"
+    CHINA = "Chiny"
 
 # Naprawione: Tylko status rozwoju rynku
 class MarketType(str, enum.Enum):
-    DEVELOPED = "DEVELOPED"
-    EMERGING = "EMERGING"
-    FRONTIER = "FRONTIER"
-    MIXED = "MIXED"
+    DEVELOPED = "Rynek rozwinięty"
+    EMERGING = "Rynek wschodzący"
+    FRONTIER = "Rynek nierozwinięty"
+    MIXED = "Mix"
 
 class DistributionPolicy(str, enum.Enum):
-    ACCUMULATING = "ACCUMULATING"
-    DISTRIBUTING = "DISTRIBUTING"
+    ACCUMULATING = "Akumulujący"
+    DISTRIBUTING = "Dystrybuujący"
 
 class ReplicationMethod(str, enum.Enum):
-    PHYSICAL = "PHYSICAL"
-    SYNTHETIC = "SYNTHETIC"
+    PHYSICAL = "Fizyczna"
+    SYNTHETIC = "Syntetyczna"
 
 class CouponFrequency(str, enum.Enum):
-    MONTHLY = "MONTHLY"
-    QUARTERLY = "QUARTERLY"
-    SEMI_ANNUALLY = "SEMI_ANNUALLY"
-    YEARLY = "YEARLY"
-    AT_THE_END = "AT_THE_END"
+    MONTHLY = "Co miesiąc"
+    QUARTERLY = "Co kwartał"
+    SEMI_ANNUALLY = "Co pół roku"
+    YEARLY = "Co roku"
+    AT_THE_END = "Przy wykupie"
 
 class InterestHandling(str, enum.Enum):
-    PAYOUT = "PAYOUT"
-    CAPITALIZATION = "CAPITALIZATION"
+    PAYOUT = "Wypłata"
+    CAPITALIZATION = "Kapitalizacja"
 
 
 # --- MIXINY (WSPÓLNE POLA DLA GIEŁDY) ---
 
 class ExchangeTradedMixin:
+    isin = db.Column(db.String, unique=True, nullable=False)
     issuer = db.Column(db.String)
     ter = db.Column(db.Numeric(6, 4))
     listing_venue = db.Column(db.String)
     domicile = db.Column(db.String)
+    spread = db.Column(db.Numeric(10, 6), default=0, nullable=False)
+
 
 
 # --- MODELE BAZY DANYCH ---
@@ -89,7 +100,6 @@ class Asset(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ticker = db.Column(db.String, unique=True, nullable=False)
     name = db.Column(db.String, nullable=False)
-    isin = db.Column(db.String, unique=True)
 
     asset_type = db.Column(db.Enum(AssetType), nullable=False)
     
@@ -97,11 +107,10 @@ class Asset(db.Model):
     category2 = db.Column(db.Enum(Category2))
     
     # Rozłączne pola geografii i typu rynku
-    geo_region = db.Column(db.Enum(GeoRegion))
-    market_type = db.Column(db.Enum(MarketType))
+    geo_region = db.Column(db.Enum(GeoRegion), nullable=False)
+    market_type = db.Column(db.Enum(MarketType), nullable=False)
 
     currency = db.Column(db.String(3), nullable=False)
-    spread = db.Column(db.Numeric(10, 6), default=0)
 
     active = db.Column(db.Boolean, default=True)
     notes = db.Column(db.Text)
@@ -115,7 +124,8 @@ class Asset(db.Model):
 
 
 class ETF(Asset, ExchangeTradedMixin):
-    __tablename__ = "etfs"
+    __tablename__ = "assets_etf"
+
     id = db.Column(db.Integer, db.ForeignKey('assets.id'), primary_key=True)
 
     benchmark = db.Column(db.String)
@@ -128,7 +138,8 @@ class ETF(Asset, ExchangeTradedMixin):
 
 
 class ETC(Asset, ExchangeTradedMixin):
-    __tablename__ = "etcs"
+    __tablename__ = "assets_etc"
+
     id = db.Column(db.Integer, db.ForeignKey('assets.id'), primary_key=True)
 
     multiplier = db.Column(db.Numeric(14, 6), default=1.0)
@@ -138,29 +149,40 @@ class ETC(Asset, ExchangeTradedMixin):
         "polymorphic_identity": AssetType.ETC
     }
 
+class EQUITY(Asset, ExchangeTradedMixin):
+    __tablename__ = "assets_akcje"
 
-class Bond(Asset):
-    __tablename__ = "bonds"
     id = db.Column(db.Integer, db.ForeignKey('assets.id'), primary_key=True)
 
-    issue_date = db.Column(db.Date)
+    __mapper_args__ = {
+        "polymorphic_identity": AssetType.EQUITY
+    }
+
+
+class Bond(Asset):
+    __tablename__ = "assets_obligacje"
+
+    id = db.Column(db.Integer, db.ForeignKey('assets.id'), primary_key=True)
+
+    retail_series_type = db.Column(db.String)
+
+    issue_date = db.Column(db.Date, nullable=False)
     maturity_date = db.Column(db.Date, nullable=False)
-    nominal_value = db.Column(db.Numeric(20, 8), nullable=False)
+    nominal_value = db.Column(db.Numeric(20, 4), nullable=False)
     
     interest_handling = db.Column(db.Enum(InterestHandling), nullable=False)
     coupon_frequency = db.Column(db.Enum(CouponFrequency))
-    is_indexed = db.Column(db.Boolean, default=False)
     
     initial_rate = db.Column(db.Numeric(6, 4))
+
+    is_indexed = db.Column(db.Boolean, default=False)
     margin = db.Column(db.Numeric(6, 4))
-    inflation_index = db.Column(db.String)
+    benchmark = db.Column(db.String)
     
-    retail_series_code = db.Column(db.String)
     early_redemption_penalty = db.Column(db.Numeric(10, 4))
     
     rating = db.Column(db.String)
-    seniority = db.Column(db.String)
-    secured = db.Column(db.Boolean, default=True)
+    secured = db.Column(db.Boolean, default=False)
 
     __mapper_args__ = {
         "polymorphic_identity": AssetType.BOND
