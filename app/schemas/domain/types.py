0d083afa34f945186a0ef7f59dc12f8750abad8b
Kuba Węgłowski <kubaweg01@gmail.com>
2026-05-09 07:54:55 +0200

    dev/removed types.py

--- app/schemas/domain/types.py
+++
@@ -1,9 +0,0 @@
-# from typing import NewType
-
-# MoneyAmount = NewType('MoneyAmount', float)
-
-# PercentTotal = NewType('PercentTotal', float)
-# PercentAnnual = NewType('PercentAnnual', float)
-
-# AssetQuantity = NewType('AssetQuantity', float)
-# FXRate = NewType('FXRate', float)
