d669b975c3ce74875c1fd0250224775aaedcb3e2
Kuba Węgłowski <kubaweg01@gmail.com>
2026-05-13 13:36:35 +0200

    clean/removed .next frontend directory

--- frontend/.next/dev/static/chunks/src_app_add_asset_page_tsx_0xpxlv3._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_024utrm._.js","static/chunks/node_modules_react-hook-form_dist_index_esm_mjs_06an-fx._.js","static/chunks/node_modules_zod_v4_0~4tofs._.js","static/chunks/node_modules_@base-ui_react_esm_03-bwg9._.js","static/chunks/node_modules_01h6h4p._.js"],
-    source: "dynamic"
-});
--- frontend/.next/dev/static/chunks/src_app_add_transaction_page_tsx_0xpxlv3._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_react-hook-form_dist_index_esm_mjs_06an-fx._.js","static/chunks/node_modules_zod_v4_0~4tofs._.js","static/chunks/node_modules_@base-ui_react_esm_01_k3lh._.js","static/chunks/node_modules_0l2g64t._.js","static/chunks/src_04qvs.3._.js"],
-    source: "dynamic"
-});
